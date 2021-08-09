cd5e2e0e00424c52ac120da37f345a0be7c3cee9
waleedsy <56700132+waleedsy@users.noreply.github.com>
2021-08-09 16:31:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
     echo 'Hello world';
     echo 'My name is Sadiq';
 ?>
-<script type="text/javascript" src="https://form.jotform.com/jsform/212204399100040"></script>
+<script type="text/javascript" src="https://form.jotform.com/jsform/212204399100040">Payment </script>
